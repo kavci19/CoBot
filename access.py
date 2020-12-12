2cfaed9b71fb7a1391a6f4c3efffb3a6e426d33f
Kaan Avci <kaanyewest1@gmail.com>
2020-12-12 13:26:42 -0500

    added get_daily_new.py to access.py

--- access.py
+++ access.py
@@ -7,6 +7,7 @@ import json
 import os
 import smtplib
 from email.message import EmailMessage
+import heapq
 
 from gs_quant.data import Dataset
 from gs_quant.session import GsSession, Environment
@@ -113,6 +114,71 @@ def get_top_5_daily_new_fatalities():
     return_countries = [country_id_name_dict[country] for country in new_fatalities['countryId'].values]
     message = '''The top 5 countries who have the highest number of new death cases today are: ''' + ', '.join(return_countries) + '.'
     return message
+
+
+# functionality: computes the countries with the most/least amount of daily new deaths/infected in a given region (if
+# supplied)
+# N = amount of countries to be returned
+# queryType = newConfirmed or newFatalities
+# region = region of the world to query (Americas, Southeast Asia, etc.)
+# order = least or greatest
+
+def get_daily_new(N, queryType, region=None, order='least'):
+
+    region_to_key = {'Americas': 'AMRO',
+                     'Eastern Mediterranean': 'EMRO',
+                     'Western Pacific': 'WPRO',
+                     'Southeast Asia': 'SEARO'}
+
+    date = str(datetime.datetime.now()).split()[0].split('-')
+
+    ds = Dataset('COVID19_COUNTRY_DAILY_WHO')
+
+    if order == 'least':
+        multiplier = -1
+    else:
+        multiplier = 1
+
+    if region:
+        if region not in region_to_key:
+            regionId = 'Other'
+        else:
+            regionId = region_to_key[region]
+
+    if region is None:
+        data = ds.get_data(datetime.date(int(date[0]), int(date[1]), int(date[2])), limit=2)
+    else:
+        data = ds.get_data(datetime.date(int(date[0]), int(date[1]), int(date[2])), regionName=regionId, limit=2)
+
+    cases = data[queryType]
+    country = data['countryName']
+
+    heap = []
+
+    for i in range(len(cases)):
+        heapq.heappush(heap, (multiplier * cases[i], country[i]))
+        if len(heap) > N:
+            heapq.heappop(heap)
+
+    heap.sort(key=lambda x: x[0])
+
+    if queryType == 'newConfirmed':
+        word = 'cases'
+    else:
+        word = 'deaths'
+
+    regionWord = ''
+    if region is not None:
+        regionWord = ' in ' + region
+
+    result = 'The ' + str(N) + ' countries with the ' + order + ' daily new ' + word + regionWord + ' are:\n'
+
+    for entry in heap:
+        result += str(entry[1]) + ': ' + str(int(abs(entry[0]))) + '\n'
+
+    return result
+
+
 
 
 test_data = ds.get_data(datetime.date(2020, 1, 21), limit=1)
